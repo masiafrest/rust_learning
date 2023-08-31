9963aacf7021e895905bee8da1e7d82dc1453532
Jzz <29693812+masiafrest@users.noreply.github.com>
2023-08-30 22:41:19 -0500

    wcr make count test work

--- command-line-oreally/wcr/src/lib.rs
+++ command-line-oreally/wcr/src/lib.rs
@@ -66,7 +66,19 @@ pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
     let mut num_words = 0;
     let mut num_bytes = 0;
     let mut num_chars = 0;
+    let mut line = String::new();
 
+    loop {
+        let line_bytes = file.read_line(&mut line)?;
+        if line_bytes == 0 {
+            break;
+        }
+        num_bytes += line_bytes;
+        num_lines += 1;
+        num_words = line.split_whitespace().count();
+        num_chars += line_bytes;
+        line.clear();
+    }
     Ok(FileInfo {
         num_lines,
         num_words,
