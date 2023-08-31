c4338def97e321701750a30ff3394d0ffb003f3a
Jzz <29693812+masiafrest@users.noreply.github.com>
2023-08-30 22:00:05 -0500

    wcr - make thinks work till page 112

--- command-line-oreally/wcr/src/lib.rs
+++ command-line-oreally/wcr/src/lib.rs
@@ -1,13 +1,15 @@
 use clap::Parser;
-// use std::error::Error;
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
-// type MyResult<T> = Result<T, Box<dyn Error>>;
+type MyResult<T> = Result<T, Box<dyn Error>>;
 
-#[derive(Parser,Debug)]
+#[derive(Parser, Debug)]
 #[command(author, version, about)]
 pub struct Config {
     /// Input file(s)
-    #[arg(default_values = ["-"])] 
+    #[arg(default_values = ["-"])]
     files: Vec<String>,
 
     /// Show word count
@@ -19,17 +21,34 @@ pub struct Config {
     bytes: bool,
 
     /// Show character count
-    #[arg(short = 'm', long, group = "cm" )]
+    #[arg(short = 'm', long, group = "cm")]
     chars: bool,
 
-    /// Show line count 
+    /// Show line count
     #[arg(short, long, default_value_t = true)]
     lines: bool,
 }
 
-pub fn get_args() ->() {
-  let matches = Config::parse();
-  println!("{:#?}", matches)
+pub fn get_args() -> MyResult<Config> {
+    let matches = Config::parse();
+    Ok(matches)
 }
 
-// pub fn run(config: Config) -> MyResult<()> {}
+pub fn run(config: Config) -> MyResult<()> {
+    for filename in &config.files {
+        match open(filename) {
+            Err(e) => eprintln!("{}, {}", filename, e),
+            Ok(_) => println!("opened {}", filename),
+        }
+    }
+
+    Ok(())
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    // println!("{}", filename);
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
