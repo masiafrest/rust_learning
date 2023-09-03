ea94a4aeda6d68d5ffa22df3ac8f9323ef18dfcf
Jzz <29693812+masiafrest@users.noreply.github.com>
2023-09-03 14:06:56 -0500

    finish unqr

--- command-line-oreally/uniqr/src/lib.rs
+++ command-line-oreally/uniqr/src/lib.rs
@@ -2,7 +2,7 @@ use clap::Parser;
 use std::{
     error::Error,
     fs::File,
-    io::{self, BufRead, BufReader},
+    io::{self, BufRead, BufReader, Write},
 };
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
@@ -20,13 +20,55 @@ pub struct Config {
     count: bool,
 }
 
+#[derive(PartialEq, Debug)]
+pub struct FileInfo {
+    num_lines: usize,
+    num_bytes: usize,
+    num_chars: usize,
+    num_words: usize,
+}
+
 pub fn get_args() -> MyResult<Config> {
     let matches = Config::parse();
     Ok(matches)
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    print!("{:?}", config);
+    let mut file = open(&config.in_file).map_err(|e| format!("{}: {}", config.in_file, e))?;
+    let mut out_file: Box<dyn Write> = match &config.out_file {
+        Some(out_name) => Box::new(File::create(out_name)?),
+        _ => Box::new(io::stdout()),
+    };
+    // know as buffer saving in memory, ill call it line
+    let mut line = String::new();
+    let mut prev_line = String::new();
+    let mut count: u64 = 0;
+
+    let mut print = |count: u64, text: &str| -> MyResult<()> {
+        if count > 0 {
+            if config.count {
+                write!(out_file, "{:>4} {}", count, text)?;
+            } else {
+                write!(out_file, "{}", text)?;
+            }
+        }
+        Ok(())
+    };
+
+    loop {
+        let bytes = file.read_line(&mut line)?;
+        if bytes == 0 {
+            break;
+        }
+        if line.trim_end() != prev_line.trim_end() {
+            print(count, &prev_line)?;
+            prev_line = line.clone();
+            count = 0;
+        }
+        count += 1;
+        line.clear()
+    }
+    print(count, &prev_line)?;
     Ok(())
 }
 
